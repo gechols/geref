0611a6043148f928466888b60b30a91b95be7811
Grant Echols <gechols@ldschurch.org>
2013-06-21 14:19:38 -0600

    Updated sample page with controller data

--- controllers/routes.js
+++ controllers/routes.js
@@ -1,4 +1,7 @@
 module.exports = function(app) {
+  app.get('/sample', function(req, res) {
+    res.render('sample', {serverDate: new Date()});
+  });
   app.get('/:page', function(req, res){
     res.render(req.params.page, {});
   });
