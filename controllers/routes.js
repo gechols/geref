3113627551c9871e5edb6f6884b2403e99ee66dc
Grant Echols <gechols@ldschurch.org>
2013-09-06 14:08:38 -0600

    Added bootstrap scafolding demo page

--- controllers/routes.js
+++ controllers/routes.js
@@ -13,6 +13,15 @@ module.exports = function(app) {
   app.get('/sample', function(req, res) {
     res.render('sample', {serverDate: new Date()});
   });
+  app.get('/bootstrap', function(req, res) {
+    res.render('bootstrap/index', {});
+  });
+  app.get('/bootstrap/withBootstrap', function(req, res) {
+    res.render('bootstrap/withBootstrap', {layout: "layouts/noLayout"});
+  });
+  app.get('/bootstrap/withoutBootstrap', function(req, res) {
+    res.render('bootstrap/withoutBootstrap', {layout: "layouts/noLayout"});
+  });
   app.get('/:page', function(req, res){
     res.render(req.params.page, {});
   });
