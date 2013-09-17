e5c233d27fda9684dfb461522755b8195fff4ad7
James Black <james@James-Black.com>
2013-09-17 16:44:22 -0600

    Fixing up clientEJS

--- assets/js/clientEjs.js
+++ assets/js/clientEjs.js
@@ -5,10 +5,8 @@ var template = "Hello world";
 $(document).ready(function () {
   console.log("Document.ready()");
   console.log("Constructing EJS");
-  require('ejs');
 //  var ejs = new EJS({});
   console.log("Rendering template");
-debugger;
   var html = ejs.render(template, {platform: "client"});
   console.log("Adding HTML:", html);
   $("#clientEJS").append(html);
