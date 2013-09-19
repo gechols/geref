5bc40f9af05cf84c1c6ea2599fa8eda31a32e474
Grant Echols <gechols@ldschurch.org>
2013-09-19 09:04:01 -0600

    Whitespace

--- assets/js/clientEjs.js
+++ assets/js/clientEjs.js
@@ -2,5 +2,6 @@ var template = "<span>This is rendered from the <%- platform %></span>";
 
 $(document).ready(function () {
   var html = ejs.render(template, {platform: "client"});
+
   $("#clientEJS").append(html);
 });
